2f69c9fa4ea2e0f05b5ef6560b1707b7be09a2cf
Tony Cheneau <tony.cheneau+gh@amnesiak.org>
2016-04-04 16:51:26 +0200

    Fix typo (colseFile -> closeFile)

--- tail.go
+++ tail.go
@@ -158,10 +158,10 @@ func (tail *Tail) Stop() error {
 
 func (tail *Tail) close() {
 	close(tail.Lines)
-	tail.colseFile()
+	tail.closeFile()
 }
 
-func (tail *Tail) colseFile() {
+func (tail *Tail) closeFile() {
 	if tail.file != nil {
 		tail.file.Close()
 		tail.file = nil
@@ -169,7 +169,7 @@ func (tail *Tail) colseFile() {
 }
 
 func (tail *Tail) reopen() error {
-	tail.colseFile()
+	tail.closeFile()
 	for {
 		var err error
 		tail.file, err = OpenFile(tail.Filename)
